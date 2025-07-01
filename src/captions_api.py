70a0fb2b82241f8016c67b9ee72a560cd5156763
Max Krolik <maxkrolik@Maxs-MacBook-Air.local>
2025-07-01 04:03:04 +0200

    changes

--- src/captions_api.py
+++ src/captions_api.py
@@ -7,15 +7,20 @@ avatars_dict = {
     2 : {"avatarId": "RuHyXqTQtifCAuHtmVMq", "avatarVariantId":"P6seXiz486l1nQid0QRT", "name": "Madison - Talking in the gym"},
     3  : {"avatarId": "B5VqhL8HPvPw8VZ4H5Uf", "avatarVariantId":"QveuIuGdwcLmtHpgGoTy", "name": "Ashley - Walking"},
     4 : {"avatarId": "lE6CMlmTY73GvrVlsbdS", "avatarVariantId":"oo0RAIESofBfKGEes2BL", "name": "Jason (Asian) - Sitting and talking"},
-    5 : {"avatarId": "XtofXWKaS7jz3nsSq5fD", "avatarVariantId":"XtofXWKaS7jz3nsSq5fD", "name": "Mia - Custom Max"},
-    6 : {"avatarId": "nYH2Gn0rgTxhwtZXyLxG", "avatarVariantId":"YuE1bYHzRL3XFB9eKPN6", "name": "Mia - Balck girl"},   
+    #5 : {"avatarId": "XtofXWKaS7jz3nsSq5fD", "avatarVariantId":"XtofXWKaS7jz3nsSq5fD", "name": "Mia - Custom Max"},
+    5 : {"avatarId": "nYH2Gn0rgTxhwtZXyLxG", "avatarVariantId":"YuE1bYHzRL3XFB9eKPN6", "name": "Mia - Balck girl"},
+    6 : {"avatarId": "ctUFWc7w2BtCwqfq4Vht", "avatarVariantId":"ygnhskO6v8ns3FGf7YmO", "name": "Violet (Asian) - Sitting in the room"},
+    7 : {"avatarId": "QaWH43qm3Otrx9HpYC4v", "avatarVariantId":"u0RHSdvb4u1eFWP6gsFG", "name": "Douglas - walking in the park"},
+    8 : {"avatarId": "18hzwOYNXUEGIFN3TUzn", "avatarVariantId":"Ug6wfqvLEpb5TeD4LHGl", "name": "Paul - walking on the beach"},
+    9 : {"avatarId": "VxQYalyR5z6CGc5vJo52", "avatarVariantId":"oU0MtgyCTw1Ban2ktyl1", "name": "Jim - sitting in the office"},
+    10 : {"avatarId": "sTknfC0E67IEebnZfsDU", "avatarVariantId":"BD0ByZURuboedGWCDiyx", "name": "Gary - walking in the park"},
 }
 
 def get_token():
     access_token = ""
     url = "https://desktop.captions.ai/api/auth/session"
     cookies = {
-        "__Secure-next-auth.session-token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..8UVrbrIfD-qhdLZ5.J8X9TIPO7ZDeFKjuDB2g5KSxMJoVqxyekPnTOnV69tdjJIcnyWghN761X6JtzHu5Rg9_Y486xaGw_DT7YlzlHh-FMkcvEtmuzUzngFfC7LlVDdMcRwGf1Kp07A5HaOGTHy7PlOpbu8kAOuptVrhmeLEIYjaMMsE9LrqniiwDokXT5_Y-V1eobr8HeBIGRTb6dd4ySzGsvYqLjv4aoZozP4a5RHztdTmvTcotQdy6a-Tuojm0HNSnqaLXseyUX3mwACnSjs9QKrd7-w-SQSY1JdeT9owVU-J88wrh_PR4MiT30_Jfcs1yDdywG1RwATR5BecSeGLZe-O4bcEHKfV_B_BBqKgseybeB79ZPGoKNEWk6urQdoNiGUdCuy9Rs_jLTeVEHFVarqAqazyccvKH6YOqTcSb323Wn3ZddOyyTV39EyrBDzDXEqqHzpbNglcGhIhEKYgxlRGDCrtO9Rg4Ey-ztdfml0jy0CPee5hJ69ST4eLVhBY2Vp9SWrLJyrcQNctDNmZofje9nYy4knWlROYfsj5qx8GO0Q58kBEKpanQo_x0_7IxmHaUykqYiCRhl0bI_23ARNN0DT_QmJrb5JCguz5sO8yv_HqwgjgKkDXQfqOBp7TVDRWX21FglQVe-QluX_7Lp7smteHbP_6T53BTYfROOQtpLOruQF3CNqcOQDBYZzBYRF5867lauWnEYEgVw5NYNjHtLTS9xBsDFD380MGjLvPDVncbLAm-GF6kXGXfnUUQy52nWKBmL-gg66brL4faQ2OEKnIkDPS5Uy8bqkoFq_Gn9kr-1nUC6iHCoGsBaLjgxGnyA5v7TNE0CVFrhedXhfZOUK8mrT4n7Q1fQNvalI3l2EzT6qXbm99jL7gcJbRaWw1LvzLh00JwI3r6fYLynrAbTWHmA_un87rGD1l2LPwewhtep0_KneF_BstAkKVSz0iGYFZtouzoOmXoe8dhI1_yHzrz1kinr87moWNvYLBIv7OjLo0vjAWO62thf3J3XRuDw11mPe9tAUO5Y8H8SmKOHqtq5G3AairrtCbtIoUlKoIpRnys3RTZheWs1tgx_PxD2pA8Zpog6r_nd4APNDxOKzHopx4VWRXavmzX9PQwjdAM75Sy-6EQQsoTbEQxqXDZTCO_cbI5sKTSLd8gXAQH2h_4CE1Vv4WrFqW-cSuetezR67f1PYQFUjlKY1X43QUPEdUL141ks5Sn4Aiw0n4rInz_AUG1ArN7QlaN7PG2NzTDhbCXkrjEX6Jlnd2TnaaCz6HkJI_EB4NoR_awfWyqk4Afqb2p_LvlEynoCMkfGqnjjMru_qo7TkHBCXKbVDUpuDPPnAgZ9ApaYk8Zg93gcbfyLErEsJg6L3vy4AjfsRUKiyY4b7RRgDBDriZ2XrbdGLP9gAV762Sj9WWW1YXZ7LVQaM5tQFsr-KZ3JaHzlWTYm13Vep7CYniRTVWgipbT5wZ_qlOIirZgpSHMFatuDJhsWpOqx4dsBdY6HiyIP0Mldwb7Uvi5x7QvMPaN3_cG0KNAasUw-KoGFBpAZsY6rTyn5leMi-YgnEbGf2Kc_SnwcSunbzcsrd2Whr9lJOCiHOcEfXmH4AVS_LhuDeqeucueaDMX0DYaB10pxxiPf6lZBeHNvekMf6cT6bU8nmy35I6DTnMd4bLtj7rZMqJNLvqyRKtVPh4BTgbPS6600psYFQEnatMWmfu0uESvPzef4k86BiyiMjeJ5brlfcY2XpQgHwj09JKeZ7Rftm0OFk8cPVTIiL6DRpLq8yKIVsnfyIael5k6C90tW7wq-Ui8SY4appmF3V6TE0q3o8g4GIe82xpOfSN8x7cpA90cElXHjTX7n8UcsTaGo8Gbdme7QUA0MIHuL73NsUW_gPWIAZ5GYV5JPcP4KjCaiN_JXUoh3zumN_Z-iQd9wVdBImWS2mzQE2RYQ_-JZGG5dkfEKzwFWFv7uEDIwxG_AIMp82IROLapJp6yHnbe004L27gWP9H_-nth7cd3RjtIEsPSQ2jSuPFzWqvx3i1xYOY3h89SSGjGs2FvwjAspJbFnac.CEltGfFkMcNSLO_ooYJzVg",
+        "__Secure-next-auth.session-token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..eOgP4U7KikYjWIDZ.yHzM96OUHuhLZsJoEJ42-ZilsQbSzeqDfiq6xEx6ghYHYNOEUW4A625VyNTpfixBQsZBQKS3p6Jue9URDrmERfiJbeFYKwNLM5gxCIr-AHbNcBRyUBrWctfyMwVKvyuF2lbEIO1tuShS5Cxb0MsYhgI5wpckRCjKmD351_RAxZ_I-ZjftWRj9CoZVccnprFIq57X8oz9xrVWqIBoXhEpK9CJugrAQThCib2d9urykdnJs6l0NFR1tlHOmq_-T5L0PliJxynSJ5iXDKtIIbqc5bIxU-wI8QwfVEkydr3xzbZ0v3zr4qbSndRxg5eVxOI9m1yJTbOsDY3UYVMLKwDLToetkcm3kZuIbdpgujboWtXLwZefOvh1bCxJUl7-YZdXUzqoC-ZkLAXlKg8iXrXz58ZoaJ6A-_Z3o6NIKiqX23eGh9l1AikEZabJ8d80Bwjj4VAB36C4Kdn63Nr46CU1LyGAhLMU-oaok78vpEKReStUs48puzMwE1fMfg-cUQBXNOInGxEYXXRDEp4mRZ-iXo73e1ocsuL2L6GfpIvLMg9Ckg6ABalwJm8Rll4VeJiRyFeA2Adz06q0Y3cokOoJOhtsf6qLVIFlFkcvsRKbrHS0sZpt_iCEqlsZ9XzFjjXagnqSby_CeX8VGRdO2rV0styHb_aA-_G7BHPrpKjX11XkofIFoH4F4Lui403ie-bzWn_UJcsdi4GHnXpQvKkkEBP_Ez-3vdPUJcJdaWxguk-GT7chFUwH9BN14tfkbkH3mXnzzeAe6yfjNSFhLQ4bBIQ4MHLq6Qae0E5KD6t9MRG994Q9uRsa2OH_dm-b9rVX2EvrzoAeICbjEVZUADtOkpDgyJPMJhKEIlwuhfI-XVsHPsM2tPmlBDFTnEQGgeN1Fu_boG6i_AgBCaI-Tvzvfx97Gbg4Q0t0QmwfxOSmt0MnuE5RoZwMKCk0KX_3lm-vyyc9JurY7ZVf0TezLKw2Ng1TydoVH3J65OXx8IqhCc-MX3UMBbc7qAdhWMvDODoZRrjT08aBivcQW1_qZKyx2quZYrhOU2ms0uigumh4PEzBIxDEruvjFfWccRfeTGgJM1Wu-MrmApGpNPxaLLm7VGr5cHInSxkZPMXoShMtsSCPzu1N2oShRREQSiGilG4luh7w5dvVz92sI1J6mk95ieopQGBURYQpe8BMQ8B1QslK-qgIp27yBM0feQyPYuRkmkVlfhqVxHh7-ZbpCk8T08Eh2rj2WWGSgVJpCQBf5iXOP80I7p7jNzp2z0Uv1iH4lar-8hyYY2jTnXKQDuAGf3D07FRYiu2L3olwiFHwW83A1mYRe0wmWnBYUB8gtz4YqpUBL50RKbxuls02QzPmMw5xJFnW-EiTzBqdBUII1rVoeEXVe8dX0hnP06XxSNMcqT0u2BgRfyrtfDbmJw_lo7cAjzb2AfEtPsWBz4pNwIXxh6bMabnIzHvjN1Zx_7TGl7NscEPGpHeXVOvohbb8Nn9AKDwuT6hCEwN_qv6TbsOhw9MAefiQjqqOvhBQRjgm8vNbNNQBW_PDL98euEVmITkWP2C7_MmWflUIlv1L4EhCYKo_pBV6gwWwvQDCE3PdUvugfyMZPuboUPyIcptwW6rpSdAWO3Am6Ois70Acin-0b_2z3pf8GkicTPYxBzcI3yuMpn-OmDdhOlIjBP4nFStPnlFG3hUN0LLmqsi8uIKpMjfBYFAx-3LIgSLVotcDQu8NlTcCkb239NKgjQGBFD0PzioFSVmk11nNBdam9A0-24cBaNaqIIrn8rVybfx0sUh0I_gewUOVI_0mTssv-QvhmIMh-cSYKCpY4PYJzNQlsml6bhHZILpVADSRO-G2kZW1a0ij4J_NmoB0dnjMaPGht0dBy4gHeQntIDIimJXdh9ag6nt2HHyp8SM34ovdlsoyvv9iJA5DfvMR5FD4Z-s_UbMMN8BcfTvq1Riid6xkpFLdR0Fb0lFWubrTWB0Xmy-goKRJgickYOYJe5Fm1x-UsUzA7gy0Q-Y3dkxdqdxEPxntafWvegPyvCahvpFs55kSKHfRJus.sdGLMZfYSZj1w0XcNTEFhg",
         #"__Host-next-auth.csrf-token": "2a0fcf698ccc46649f766e22743d679abe11df40e5fc9dd27fd6163abb62ca69",
         #"__Secure-next-auth.callback-url": "https%3A%2F%2Fdesktop.captions.ai%2Fprojects%3Fprovider%3Dmicrosoft%26stableId%3DB_jzPRiROqDc6TasXYvqn",
         #"stableId": "B_jzPRiROqDc6TasXYvqn",
@@ -121,7 +126,12 @@ def display_avatars():
     print("")
     for k, v in avatars_dict.items():
         print(f"{k}. {v['name']}")
+    print(f"{k+1}. Use custom avatar (You need: avatarId, avatarVariantId)")
     chosen_id = int(input("\nWhich avatar you'd like to use? (type number): "))
+    if chosen_id == len(avatars_dict) + 1:
+        avatar_id = input("Enter avatarId: ")
+        avatar_variant_id = input("Enter avatarVariantId: ")
+        avatars_dict[len(avatars_dict)+1] = {"avatarId": avatar_id, "avatarVariantId": avatar_variant_id, "name": f"Custom Avatar {len(avatars_dict)}"}
     return chosen_id
 
 
